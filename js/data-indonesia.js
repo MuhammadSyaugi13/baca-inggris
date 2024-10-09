2dca56d9325e7b05cedaa87b9279a38bfb93ea26
goit <goit@gmail.com>
2024-10-09 19:12:10 +0700

    menambahkan teks

--- js/data-indonesia.js
+++ js/data-indonesia.js
@@ -9,5 +9,27 @@ const dataIndonesia = [
 
     "Sungguh luar biasa, betapa semuanya bersemi pada musim ini! Apa yang membuat limpahan warna hijau dedaunan bermunculan dari bumi yang mati begitu ia mendapatkan kehangatan dan sisa-sisa salju yang terakhir menghilang?",
 
-    "Ketika Sophie membuka pintu gerbang halamannya, dia memandang ke kotak surat. Biasanya ada banyak surat sampah dan beberapa amplop besar untuk ibunya. Tumpukan surat itu sering ditinggalkannya begitu saja di meja dapur sebelum dia naik ke kamar untuk mulai mengerjakan pekerjaan rumah."
+    "Ketika Sophie membuka pintu gerbang halamannya, dia memandang ke kotak surat. Biasanya ada banyak surat sampah dan beberapa amplop besar untuk ibunya. Tumpukan surat itu sering ditinggalkannya begitu saja di meja dapur sebelum dia naik ke kamar untuk mulai mengerjakan pekerjaan rumah.",
+
+    "Sering kali ada beberapa surat dari bank untuk ayahnya. Tetapi, ayah Sophie bukanlah seorang ayah yang biasa. Dia adalah nakhoda sebuah tanker minyak besar, dan selalu bepergian hampir sepanjang tahun. Selama beberapa minggu ketika dia berada di rumah, dia akan sibuk ke sana kemari untuk membuat rumah itu enak dan nyaman bagi Sophie dan ibunya. Namun jika dia berada di laut, dia tampaknya sangat jauh.",
+
+    "Hanya ada sebuah surat di kotak surat—dan itu adalah untuk Sophie. Pada amplop putih tertulis: 'Sophie Amundsend, 3 Clover Close.' Itu saja; tidak disebutkan siapa pengirimnya. Prangkonya puntidak ada",
+
+    " Setelah Sophie menutup pintu gerbang, dia buru-buru membuka amplop itu. Di dalamnya hanya ada secarik kertas yang tidak lebih besar daripada amplopnya sendiri. Bunyinya: Siapakah kamu?",
+
+    "Tidak ada yang lain, hanya dua kata itu, yang ditulis tangan, dan diikuti dengan sebuah tanda tanya besar.",
+
+    " Dia melihat amplop itu lagi. Surat itu jelas untuknya. Siapakah yang memasukkannya ke dalam kotak surat?",
+
+    " Sophie segera memasuki rumah merah itu. Sebagaimana biasa, kucingnya, Sherekan, berusaha menyelinap ke luar dari semak-semak, melompat ke tangga pertama, dan menyusup masuk melalui pintu sebelum Sophie menutupnya.",
+
+    " Setiap kali ibu Sophie sedang tidak enak hati, dia akan menyebut rumah yang mereka tinggali itu adalah sebuah kandang. Sophie memang suka memelihara binatang. Pertama-tama dia punya tiga ekor ikan mas, Goldtop, Red Ridinghood, dan Black Jack. Selanjutnya dia mendapatkan dua ekor burung parkit yang dinamakannya Smitt dan Smule, lalu Govinda si kura-kura darat, dan akhirnya si kucing pirang, Sherekan. Semua binatang itu diberikan kepadanya untuk menghiburnya, mengingat bahwa ibunya selalu baru pulang kerja menjelang senja dan ayahnya yang sangat sering bepergian, berlayar ke seluruh penjuru dunia.",
+
+    "Sophie melemparkan tas sekolahnya ke lantai dan meletakkan semangkuk makanan kucing untuk Sherekan. Lalu, dia duduk di atas bangku dapur dengan surat misterius di tangannya. Siapakah kamu ?",
+
+    "Dia tidak tahu. Dia adalah Sophie Amundsend, tentu saja, tapi siapakah Sophie itu? Dia benar-benar tidak mengerti—belum.",
+
+    "Bagaimana seandainya dia telah diberi nama lain? Anne Knutsen, misalnya. Apakah dia lalu menjadi orang lain?",
 ]
+
+// page 21
--- js/data-inggris.js
+++ js/data-inggris.js
@@ -9,5 +9,27 @@ const dataInggris = [
     
     "It was extraordinary how everything burst forth at this time of year! What madethis great mass of green vegetation come welling up from the dead earth as soon as itgot warm and the last traces of snow disappeared?",
     
-    "As Sophie opened her garden gate, she looked in the mailbox. There wasusually a lot of junk mail and a few big envelopes for her mother, a pile to dump onthe kitchen table before she went up to her room to start her homework."
+    "As Sophie opened her garden gate, she looked in the mailbox. There wasusually a lot of junk mail and a few big envelopes for her mother, a pile to dump onthe kitchen table before she went up to her room to start her homework.",
+
+    "From time to time there would be a few letters from the bank for her father, but then he was not a normal father. Sophie’s father was the captain of a big oil tanker, and was away for most of the year. During the few weeks at a time when he was at home, he would shuffle around the house making it nice and cozy for Sophie and her mother. But when he was at sea he could seem very distant.",
+    
+    "There was only one letter in the mailbox—and it was for Sophie. The white envelope read: “Sophie Amundsen, 3 Clover Close.” That was all; it did not say who it was from. There was no stamp on it either. ",
+
+    "As soon as Sophie had closed the gate behind her she opened the envelope. It contained only a slip of paper no bigger than the envelope. It read: Who are you?",
+
+    "Nothing else, only the three words, written by hand, and followed by a large question mark.",
+
+    "She looked at the envelope again. The letter was definitely for her. Who could have dropped it in the mailbox?",
+
+    "Sophie let herself quickly into the red house. As always, her cat Sherekan managed to slink out of the bushes, jump onto the front step, and slip in through the door before she closed it behind her.",
+
+    "Whenever Sophie’s mother was in a bad mood, she would call the house they lived in a menagerie. A menagerie was a collection of animals. Sophie certainly had one and was quite happy with it. It had begun with the three goldfish, Goldtop, Red Ridinghood, and Black Jack. Next she got two budgerigars called Smitt and Smule, then Govinda the tortoise, and finally the marmalade cat Sherekan. They had all been given to her to make up for the fact that her mother never got home from work until late in the afternoon and her father was away so much, sailing all over the world.",
+
+    "Sophie slung her schoolbag on the floor and put a bowl of cat food out for Sherekan. Then she sat down on a kitchen stool with the mysterious letter in her hand. Who are you?",
+
+    "She had no idea. She was Sophie Amundsen, of course, but who was that? She had not really figured that out—yet.",
+
+    "Bagaimana seandainya dia telah diberi nama lain? Anne Knutsen, misalnya. Apakah dia lalu menjadi orang lain?"
 ]
+
+// page 4
