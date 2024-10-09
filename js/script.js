4d4b7d0296e171efb6ba408b87a33904fab7c7c3
goit <goit@gmail.com>
2024-10-09 18:56:59 +0700

    memberikan session

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 const englishArea = document.getElementById("english-area")
 const indonesiaArea = document.getElementById("indonesia-area")
+let position = localStorage.getItem("position")
 
-{/* <div class="area-text my-2">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos enim provident sequi, ut consequuntur quod eum dolorum rem esse accusantium.
-</div> */}
 
 let englishText = ''
 dataInggris.forEach((text, i) => {
     
-    englishText += `<div class="area-text ${(i % 2) ? 'read-area' : 'read-area-odd'} my-2">
+    englishText += `<div class="area-text ${(i % 2) ? 'read-area' : 'read-area-odd'} my-3">
                         > ${text}
                     </div>`
     
@@ -18,20 +16,23 @@ englishArea.innerHTML = englishText
 
 let indonesiaText = ''
 dataIndonesia.forEach((text, i) => {
-    indonesiaText += `<div class="area-text ${(i % 2) ? 'read-area' : 'read-area-odd'} my-2">
+    indonesiaText += `<div class="area-text ${(i % 2) ? 'read-area' : 'read-area-odd'} my-3">
                         > ${text}
                     </div>`
     
 })
 
+englishArea.scrollTop = (position) ? parseInt(position) : null
+indonesiaArea.scrollTop = (position) ? parseInt(position) : null
+
+
 indonesiaArea.innerHTML = indonesiaText
 
 
 englishArea.addEventListener('scroll', (e) => {
-    console.log(e);
-    console.log(englishArea.scrollTop);
 
     indonesiaArea.scrollTop = englishArea.scrollTop
+    localStorage.setItem("position", englishArea.scrollTop);
 })
 
 
